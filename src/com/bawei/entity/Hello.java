fe54a95733626da2067ff929a60767663e633caa
bobo <348828809@qq.com>
2019-07-10 16:53:02 +0800

    冲突又解决了

--- src/com/bawei/entity/Hello.java
+++ src/com/bawei/entity/Hello.java
@@ -9,6 +9,10 @@ public class Hello {
 		System.out.println("你好");
 
 		System.out.println("本机改变了");
+		System.out.println("本机又改变了");
 		System.out.println("服务器改变了");
+		
+		System.out.println("服务器改变了2222222");
+		
 	}
 }
